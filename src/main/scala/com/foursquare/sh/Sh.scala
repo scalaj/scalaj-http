7f4b7204046165fcc0d9fdb2f9f1c0439cc88164
Jon Hoffman <jonhoffman@gmail.com>
2010-05-25 03:08:08 -0400

    added oauth

--- src/main/scala/com/foursquare/sh/Sh.scala
+++ src/main/scala/com/foursquare/sh/Sh.scala
@@ -1,6 +1,6 @@
 package com.foursquare.sh
 
-import java.net.{HttpURLConnection, URL, URLEncoder}
+import java.net.{HttpURLConnection, URL, URLEncoder, URLDecoder}
 import java.io.{DataOutputStream, InputStream, BufferedReader, InputStreamReader}
 import org.apache.commons.codec.binary.Base64
 
@@ -14,7 +14,84 @@ object ShOptions {
   
 }
 
+class ShException(val code: Int, message: String) extends RuntimeException("ResponseCode[" + code + "] " + message)
 
+case class Token(key: String, secret: String)
+object OAuth {
+  import java.net.URI
+  import javax.crypto.Mac
+  import javax.crypto.SecretKey
+  import javax.crypto.spec.SecretKeySpec
+  val MAC = "HmacSHA1"
+      
+  def sign(req: Sh.Request, consumer: Token, token: Option[Token], verifier: Option[String]): Sh.Request = {
+
+    
+    val baseParams:List[(String,String)] = List(
+      ("oauth_timestamp", (System.currentTimeMillis/1000).toString),
+      ("oauth_nonce",System.currentTimeMillis.toString)
+    )
+    
+    var (oauthParams, signature) = getSig(baseParams, req, consumer, token, verifier)
+    
+    oauthParams ::= ("oauth_signature", signature)
+    
+    req.header("Authorization", "OAuth " + oauthParams.map(p => p._1 + "=\"" + percentEncode(p._2) +"\"").mkString(","))
+  }
+  
+  def getSig(baseParams: List[(String,String)], req: Sh.Request, consumer: Token, token: Option[Token], verifier: Option[String]): (List[(String,String)], String) = {
+    var oauthParams = ("oauth_version", "1.0")::("oauth_consumer_key", consumer.key)::("oauth_signature_method", "HMAC-SHA1") :: baseParams
+    
+    token.foreach{t =>
+      oauthParams ::= ("oauth_token", t.key)
+    }
+    
+    verifier.foreach{v =>
+      oauthParams ::= ("oauth_verifier", v)
+    }
+    
+    val baseString = List(req.method.toUpperCase,normalizeUrl(req.url(req)),normalizeParams(req.params ++ oauthParams)).map(percentEncode).mkString("&")
+    
+    val keyString = percentEncode(consumer.secret) + "&" + token.map(t => percentEncode(t.secret)).getOrElse("")
+    val key = new SecretKeySpec(keyString.getBytes(Sh.charset), MAC)
+    val mac = Mac.getInstance(MAC)
+    mac.init(key)
+    val text = baseString.getBytes(Sh.charset)
+    (oauthParams, Sh.base64(mac.doFinal(text)))
+  }
+  
+  private def normalizeParams(params: List[(String,String)]) = {
+    percentEncode(params).sort(_ < _).mkString("&")
+  }
+  
+  private def normalizeUrl(url: URL) = {
+    val uri = new URI(url.toString)
+    val scheme = uri.getScheme().toLowerCase()
+    var authority = uri.getAuthority().toLowerCase()
+    val dropPort = (scheme.equals("http") && uri.getPort() == 80) || (scheme.equals("https") && uri.getPort() == 443)
+    if (dropPort) {
+      // find the last : in the authority
+      val index = authority.lastIndexOf(":")
+      if (index >= 0) {
+        authority = authority.substring(0, index)
+      }
+    }
+    var path = uri.getRawPath()
+    if (path == null || path.length() <= 0) {
+      path = "/" // conforms to RFC 2616 section 3.2.2
+    }
+    // we know that there is no query and no fragment here.
+    scheme + "://" + authority + path
+  }
+  
+  def percentEncode(params: List[(String,String)]):List[String] = params.map(p => percentEncode(p._1) + "=" + percentEncode(p._2))
+  
+  def percentEncode(s: String): String = {
+    if (s == null) "" else {
+       Sh.urlEncode(s).replace("+", "%20").replace("*", "%2A").replace("%7E", "~")
+     }
+  }
+}
 
 object Sh {
   def apply(url: String):Request = get(url)
@@ -23,19 +100,30 @@ object Sh {
   type ShUrl = Request => URL
   
   object Request {
-    def apply(exec: ShExec, url: ShUrl, method:String):Request = Request(exec, url, Nil, Nil, ShOptions.method(method)::defaultOptions)
+    def apply(exec: ShExec, url: ShUrl, method:String):Request = Request(method, exec, url, Nil, Nil, ShOptions.method(method)::defaultOptions)
   }
-  case class Request(exec: ShExec, url: ShUrl, params: List[(String,String)], headers: List[(String,String)], options: List[ShOptions.ShOption]) {
-    def params(p: List[(String,String)]):Request = Request(exec,url, p, headers,options)
-    def headers(h: List[(String,String)]):Request = Request(exec,url, params, h,options)
-    def param(key: String, value: String):Request = Request(exec,url,(key,value)::params, headers,options)
-    def header(key: String, value: String):Request = Request(exec,url,params, (key,value)::headers,options)
-    def options(o: List[ShOptions.ShOption]):Request = Request(exec, url, params, headers, o)
-    def option(o: ShOptions.ShOption):Request = Request(exec,url, params, headers,o::options)
+  case class Request(method: String, exec: ShExec, url: ShUrl, params: List[(String,String)], headers: List[(String,String)], options: List[ShOptions.ShOption]) {
+    def params(p: List[(String,String)]):Request = Request(method, exec,url, p, headers,options)
+    def headers(h: List[(String,String)]):Request = Request(method,exec,url, params, h,options)
+    def param(key: String, value: String):Request = Request(method,exec,url,(key,value)::params, headers,options)
+    def header(key: String, value: String):Request = Request(method,exec,url,params, (key,value)::headers,options)
+    def options(o: List[ShOptions.ShOption]):Request = Request(method,exec, url, params, headers, o)
+    def option(o: ShOptions.ShOption):Request = Request(method,exec,url, params, headers,o::options)
     
     def auth(user: String, password: String) = header("Authorization", "Basic " + base64(user + ":" + password))
     
+    def oauth(consumer: Token) = OAuth.sign(this, consumer, None, None)
+    def oauth(consumer: Token, token: Token) = OAuth.sign(this, consumer, Some(token), None)
+    def oauth(consumer: Token, token: Token, verifier: String) = OAuth.sign(this, consumer, Some(token), Some(verifier))
+    
+    def tryParse[E](is: InputStream, parser: InputStream => E):E = try {
+      parser(is)
+    } finally {
+      is.close
+    }
+    
     def apply[T](parser: InputStream => T): T = {
+
       url(this).openConnection match {
         case conn:HttpURLConnection =>
           conn.setInstanceFollowRedirects(true)
@@ -45,36 +133,48 @@ object Sh {
           options.reverse.foreach(_(conn))
 
           exec(this, conn)
-          val is = conn.getInputStream()
           try {
-            parser(is)
-          } finally {
-            is.close
+            tryParse(conn.getInputStream(), parser)
+          } catch {
+            case e: java.io.IOException =>
+              throw new ShException(conn.getResponseCode, tryParse(conn.getErrorStream(), readString))
           }
+
       }
     }
     
-    def asString = apply(is => {
-      
+    def readString(is: InputStream) = {
       val sb = new StringBuilder()
       val reader = new BufferedReader(new InputStreamReader(is, charset))
       var ch = -1
-      def read = {
+      def read() = {
         ch = reader.read()
         (ch != -1)
       }
-      while(read())
+      while(read()){
         sb.append(ch.asInstanceOf[Char])
       }
       sb.toString
-    })
+    }
+    
+    def asString = apply(readString)
     
     def asXml = apply(is => scala.xml.XML.load(is))
+    
+    def asParams: List[(String,String)] = {
+      asString.split("&").flatMap(_.split("=") match {
+        case Array(k,v) => Some(urlDecode(k), urlDecode(v))
+        case _ => None
+      }).toList
+    }
+    
+    def asParamMap = Map(asParams:_*)
   }
 
   val defaultOptions = List(ShOptions.connTimeout(30000), ShOptions.readTimeout(60000))
   
   def urlEncode(name: String): String = URLEncoder.encode(name, charset)
+  def urlDecode(name: String): String = URLDecoder.decode(name, charset)
   def base64(bytes: Array[Byte]) = new String((new Base64).encode(bytes))
   def base64(in: String): String = base64(in.getBytes(charset))
   
@@ -97,7 +197,7 @@ object Sh {
       conn.connect
       conn.getOutputStream.write(toQs(req.params).getBytes(charset))
     }
-    Request(postFunc, noopShUrl(url), "POST")
+    Request(postFunc, noopShUrl(url), "POST").header("content-type", "application/x-www-form-urlencoded")
   }
   val charset = "UTF-8"
 }
--- src/test/scala/com/foursquare/sh/ShSpec.scala
+++ src/test/scala/com/foursquare/sh/ShSpec.scala
@@ -4,6 +4,20 @@ import org.specs._
 
 class ShSpec extends Specification {
   "Sh" should {
-    "work" in { false must be(true) }
+    "oauth sign correctly from example http://hueniverse.com/2008/10/beginners-guide-to-oauth-part-iv-signing-requests/" in {
+      val params = List(
+          ("oauth_nonce","kllo9940pd9333jh"),
+          ("oauth_timestamp","1191242096")
+      )
+      
+      val url = "http://photos.example.net/photos"
+      val req = Sh(url).param("file", "vacation.jpg").param("size", "original")
+      
+      val (oauthParams, signature) = OAuth.getSig(params, req, Token("dpf43f3p2l4k3l03","kd94hf93k423kf44"), Some(Token("nnch734d00sl2jdk","pfkkdhi9sl3r4s00")),None)
+      
+      signature must_== "tR3+Ty81lMeYAr/Fid0kMTYa/WM="
+    }
   }
+  
+  
 }
