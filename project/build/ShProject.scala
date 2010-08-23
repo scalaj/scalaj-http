df8051faf3f2fd62076cf6f32174afae38777e0e
Jon Hoffman <jonhoffman@gmail.com>
2010-08-23 14:50:55 -0400

    cross compile

--- project/build/ShProject.scala
+++ project/build/ShProject.scala
@@ -2,7 +2,13 @@ import sbt._
 import sbt.Process._
 
 class ShProject(info: ProjectInfo) extends DefaultProject(info) {
+  override def managedStyle = ManagedStyle.Maven
   
   val commonsCodec = "commons-codec" % "commons-codec" % "1.3" % "compile" withSources()
-  val specs  = "org.scala-tools.testing" % "specs" % "1.6.2.1"  % "test" withSources()
+  
+  val specs = if (buildScalaVersion.startsWith("2.7.")) {
+    "org.scala-tools.testing" % "specs" % "1.6.2.2" % "test" withSources()
+  } else {
+    "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources()
+  }
 }
