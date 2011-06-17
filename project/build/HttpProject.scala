0b347d531c5349002830a8a5f046b0f9fe3fa9d0
Jorge Ortiz <jorge.ortiz@gmail.com>
2011-06-17 16:56:05 -0400

    Cross-compile to more Scala versions

--- project/build/HttpProject.scala
+++ project/build/HttpProject.scala
@@ -11,11 +11,11 @@ class HttpProject(info: ProjectInfo) extends DefaultProject(info) {
   
   val commonsCodec = "commons-codec" % "commons-codec" % "1.4" % "compile" withSources()
   
-  val specs = if (buildScalaVersion.startsWith("2.7.")) {
-    "org.scala-tools.testing" % "specs" % "1.6.2.2" % "test" withSources()
-  } else {
-    "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources()
+  val specs = buildScalaVersion match {
+    case v if v.startsWith("2.7.") => "org.scala-tools.testing" % "specs" % "1.6.2.2" % "test" withSources()
+    case "2.8.0" => "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources()
+    case "2.8.1" | "2.9.0-1" => "org.scala-tools.testing" %% "specs" % "1.6.8" % "test" withSources()
   }
-  
-  val dispatchOauth    = "net.databinder" %% "dispatch-oauth"     % "0.7.5" % "test"
+
+  val dispatchOauth    = "net.databinder" %% "dispatch-oauth"     % "0.7.8" % "test"
 }
