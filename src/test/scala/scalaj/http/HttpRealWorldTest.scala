62976d9b3553703021f6a15ca9b6bb008bb66115
Jiří Helmich <jiri@helmich.cz>
2016-02-09 13:19:14 +0100

    Failing real-world test for followRedirects

--- src/test/scala/scalaj/http/HttpRealWorldTest.scala
+++ src/test/scala/scalaj/http/HttpRealWorldTest.scala
@@ -9,6 +9,16 @@ class HttpRealWorldTest {
     val response = Http("https://twitter.com").asString
     assertEquals(200, response.code)
     assertEquals(Some("gzip"), response.header("content-encoding"))
-    assertEquals("<!DOCTYPE", response.body.substring(0,9))
+    assertEquals("<!DOCTYPE", response.body.substring(0, 9))
+  }
+
+  @Test
+  def followRedirect {
+    val response = Http("http://purl.org/linked-data/sdmx/2009/code#sex-M")
+      .header("Accept", "text/turtle")
+      .option(HttpOptions.followRedirects(true))
+      .asString
+    assertEquals(200, response.code)
+    assertTrue(response.body.contains("sdmx-code:unitMult"))
   }
 }
